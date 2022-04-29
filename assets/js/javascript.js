fb96c3b259c3c88915334a4436ebb7c0229c4b24
kstambaugh <refinancethekraken@gmail.com>
2022-04-29 15:12:01 -0400

    finalized homework

---
+++ assets/js/javascript.js
@@ -0,0 +1,18 @@
+
+window.onscroll = function () { myFunction() };
+
+
+var header = document.getElementById("myBanner");
+
+
+var sticky = header.offsetTop;
+
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
